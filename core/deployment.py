bcc41217017014c6795d7065173b55064462f67d
yousifmohame <me8999109@gmail.com>
2025-07-07 11:47:16 +0300

    adding ultramsg instance id

--- core/deployment.py
+++ core/deployment.py
@@ -5,6 +5,7 @@ from .settings import BASE_DIR
 
 SECRET_KEY = os.environ['SECRET']
 GOOGLE_API_KEY = os.environ['GOOGLE_API_KEY']
+ULTRAMSG_INSTANCE_ID = os.environ['ULTRAMSG_INSTANCE_ID']
 
 ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']]
 CSRF_TRUSTED_ORIGINS = ['https://'+ os.environ['WEBSITE_HOSTNAME']]
