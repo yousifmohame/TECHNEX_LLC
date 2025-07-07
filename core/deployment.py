7723eada1499a17e653a27559415da34f4846553
yousifmohame <me8999109@gmail.com>
2025-07-07 11:43:18 +0300

    adding secret key

--- core/deployment.py
+++ core/deployment.py
@@ -2,10 +2,10 @@ import os
 from .settings import *
 from .settings import BASE_DIR
 
+
+SECRET_KEY = os.environ['SECRET']
 ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']]
-
 CSRF_TRUSTED_ORIGINS = ['https://'+ os.environ['WEBSITE_HOSTNAME']]
-
 DEBUG = False
 
 MIDDLEWARE = [
